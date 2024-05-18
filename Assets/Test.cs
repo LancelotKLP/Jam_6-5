03495c24f91d4949d7dc024deeec897725d07686
Corentin METZ-DUBOIS <corentin.mzds@gmail.com>
2024-05-18 13:30:46 +0200

    time

---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] Text datetimeText;
+
+    void Update()
+    {
+        if (WorldTimeAPI.Instance.isTimeLoaded) {
+            DateTime currentDateTime = WorldTimeAPI.Instance.GetCurrentDateTime();
+
+            datetimeText.text = currentDateTime.ToString();
+        }
+    }
+}
